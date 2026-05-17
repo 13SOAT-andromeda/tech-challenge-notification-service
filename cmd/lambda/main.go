70fb547f3c78280b4a95ddd45ca20e97bfce30a1
JulioCVaz <julio.oliveiravaz0@gmail.com>
2026-05-17 19:44:57 -0300

    chore: stub main.go and tidy go modules after removing old service layer

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -1,16 +1,3 @@
 package main
 
-import (
-	"github.com/aws/aws-lambda-go/lambda"
-
-	adapterlambda "github.com/JulioCVaz/tech-challenge-notification-service/internal/adapter/lambda"
-	"github.com/JulioCVaz/tech-challenge-notification-service/internal/adapter/logger"
-	"github.com/JulioCVaz/tech-challenge-notification-service/internal/application/services"
-)
-
-func main() {
-	sender := logger.NewLogSender()
-	svc := services.NewNotificationService(sender)
-	handler := adapterlambda.NewHandler(svc)
-	lambda.Start(handler.Handle)
-}
+func main() {}
